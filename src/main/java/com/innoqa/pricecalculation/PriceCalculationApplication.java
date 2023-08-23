4afcab01c1abc3b75161c0f966f8efc45655ad38
AngelaCastillo <angiekroll@gmail.com>
2023-08-23 14:56:00 -0500

    ✨ feat(pricecalculation): first commit

---
+++ src/main/java/com/innoqa/pricecalculation/PriceCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.innoqa.pricecalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceCalculationApplication.class, args);
+	}
+
+}
